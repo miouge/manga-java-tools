4eee2a6b1455e09271dc13cb16a17314eb423e3e
pri <p.ripoll@envea.global>
2021-05-28 14:11:38 +0200

    Analyse module : add splitting functionnality (not finished)

--- manga-tools/src/programs/Analyse.java
+++ manga-tools/src/programs/Analyse.java
@@ -12,6 +12,9 @@ import java.util.TreeSet;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.math.fraction.Fraction;
+import org.apache.commons.math.fraction.FractionFormat;
+
 import beans.Config;
 import beans.FileItem;
 import beans.Tools;
@@ -34,7 +37,104 @@ public class Analyse {
 	static int excludeWidthGreaterThan = -1;
 	static int excludeHeightLessThan = -1;
 	static int excludeHeightGreaterThan = -1;
-		 
+
+
+	//  0     X1        X2      X3           X4   Width
+	//        |          |       |            |
+	//  Y1 ----------------------------------------
+	//        |          |       |            |
+	//        |          |       |            |
+	//        |   left   |       |   right    |
+	//        |   half   |       |   half     |
+	//        |          |       |            |
+	//        |          |       |            |
+	//  Y2 ----------------------------------------
+	//        |          |       |            |
+	//  Height
+	
+	static int splitDoublePageImage = 0;
+	static float splitIfRatioGreaterThan = 99F;
+	static int firstPageIsRightHalf = 1;	
+	static double splitY1Ratio = 0.0;
+	static double splitY2Ratio = 1.0;
+	static double splitX1Ratio = 0.0;
+	static double splitX2Ratio = 0.5;
+	static double splitX3Ratio = 0.5;
+	static double splitX4Ratio = 1.0;	
+	
+	static void splitImage( BufferedImage srcImage, FileItem fi, String destFolder ) throws Exception {
+		
+		int width = srcImage.getWidth();
+		int height = srcImage.getHeight();
+		
+		int leftX = 0;
+		int rightX = width/2;
+		
+		int leftW = rightX - leftX;		
+		int rightW = width - rightX;
+		
+		int leftY = (int)Math.floor((double)height * splitY1Ratio);
+		int rightY = leftY;  
+		
+		int leftH = (int)Math.ceil((double)height * splitY2Ratio) - leftY;
+		int rightH = leftH; 
+				
+		BufferedImage leftHalf  = srcImage.getSubimage( leftX, leftY, leftW, leftH );
+		BufferedImage rightHalf = srcImage.getSubimage( rightX, rightY, rightW, rightH );
+		
+		// auto detect the image format from the file it's extension
+		String format = null;
+		String fileName = fi.name;
+		
+		String[] parts = fileName.split("\\.");
+		
+		if( parts.length >= 2 ) {
+
+			String ext = parts[ parts.length - 1 ].toLowerCase();
+			switch( ext ) {
+				case "jpeg" :
+				case "jpg" : { format = "jpg"; break; }
+				case "png" : { format = "png"; break; }
+			}
+		}
+		
+		String filenameL = "";
+		String filenameR = "";
+		
+		for( int i = 0 ; i < (parts.length-1) ; i++ ) {
+			
+			filenameL += parts[i];
+			filenameR += parts[i];
+		}
+		
+		if( firstPageIsRightHalf == 1 ) {
+			
+			filenameL += "b" + "." + format;
+			filenameR += "a" + "." + format;
+			
+		} else {
+			
+			// first page is left half
+			filenameL += "a" + "." + format;
+			filenameR += "b" + "." + format;
+		}
+
+		if( format == null ) {
+			throw new Exception( " unable to detect the image format ");
+		}
+		
+		File outputfileL = new File( destFolder + "/" + filenameL );
+		File outputfileR = new File( destFolder + "/" + filenameR );
+		ImageIO.write( leftHalf , format, outputfileL );
+		ImageIO.write( rightHalf, format, outputfileR );
+	}
+	
+	static boolean doSplitImage( BufferedImage srcImage ) throws Exception  {
+		
+		
+		return true;
+	}	
+	
 	static boolean doExcludeImage( BufferedImage srcImage ) throws Exception  {
 
 		int width = srcImage.getWidth();
@@ -63,7 +163,6 @@ public class Analyse {
 		File file = new File( fi.fullpathname );
 		BufferedImage srcImage = ImageIO.read( file );
 		
-		String destinationPath;
 		
 		if( doExcludeImage( srcImage ) ) {
 
@@ -76,14 +175,19 @@ public class Analyse {
 				excludeFolderCreated = true;
 			}
 			
-			destinationPath = excludeFolder + "/" + fi.name;
+			String destinationPath = excludeFolder + "/" + fi.name;
+			Files.copy(Paths.get( fi.fullpathname), Paths.get( destinationPath), StandardCopyOption.REPLACE_EXISTING );			
 			excludedCount++;
 		}
+		else if( doSplitImage( srcImage ) ) {
+			
+			String destinationPath = config.analysedFolder + "/" + String.format( subFolderFmt, volumeNo );
+			splitImage( srcImage, fi, destinationPath );
+		}
 		else {
-			destinationPath = config.analysedFolder + "/" + String.format( subFolderFmt, volumeNo ) + "/" + fi.name;
+			String destinationPath = config.analysedFolder + "/" + String.format( subFolderFmt, volumeNo ) + "/" + fi.name;
+			Files.copy(Paths.get( fi.fullpathname), Paths.get( destinationPath), StandardCopyOption.REPLACE_EXISTING );
  		}
-		
-		Files.copy(Paths.get( fi.fullpathname), Paths.get( destinationPath), StandardCopyOption.REPLACE_EXISTING );
 		
 		widths.add(srcImage.getWidth());
 		heights.add(srcImage.getHeight());
@@ -98,7 +202,27 @@ public class Analyse {
 		excludeWidthLessThan     = Integer.parseInt( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "excludeWidthLessThan"     , "-1" ));
 		excludeWidthGreaterThan  = Integer.parseInt( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "excludeWidthGreaterThan"  , "-1" ));
 		excludeHeightLessThan    = Integer.parseInt( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "excludeHeightLessThan"    , "-1" ));
-		excludeHeightGreaterThan = Integer.parseInt( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "excludeHeightGreaterThan" , "-1" ));				
+		excludeHeightGreaterThan = Integer.parseInt( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "excludeHeightGreaterThan" , "-1" ));
+		
+		splitDoublePageImage     = Integer.parseInt( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "splitDoublePageImage"     , "0" ));
+		splitIfRatioGreaterThan  = Float.parseFloat( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "splitIfRatioGreaterThan"  , "99" ));
+		firstPageIsRightHalf     = Integer.parseInt( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "firstPageIsRightHalf"     , "1" ));
+		
+		FractionFormat ff = new FractionFormat();
+		Fraction fraction;
+
+		fraction = ff.parse( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "splitY1Ratio", "0/100" ) );
+		splitY1Ratio = fraction.doubleValue();
+		fraction = ff.parse( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "splitY2Ratio", "100/100" ) );
+		splitY2Ratio = fraction.doubleValue();
+		fraction = ff.parse( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "splitX1Ratio", "0/100" ) );
+		splitX1Ratio = fraction.doubleValue();
+		fraction = ff.parse( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "splitX2Ratio", "50/100" ) );
+		splitX2Ratio = fraction.doubleValue();
+		fraction = ff.parse( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "splitX3Ratio", "50/100" ) );
+		splitX3Ratio = fraction.doubleValue();
+		fraction = ff.parse( Tools.getIniSetting( Config.settingsFilePath, "Analyse", "splitX4Ratio", "100/100" ) );
+		splitX4Ratio = fraction.doubleValue();
 	}	
 	
 	public static void checkOriginalImages() throws Exception {
